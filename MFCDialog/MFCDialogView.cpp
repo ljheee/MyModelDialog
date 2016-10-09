a55627af15c7e20f22bcaa625bd576144f259e1f
ljh_acer <554278334@qq.com>
2016-10-09 13:32:40 +0800

    create

---
+++ MFCDialog/MFCDialogView.cpp
@@ -0,0 +1,104 @@
+
+// MFCDialogView.cpp : CMFCDialogView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFCDialog.h"
+#endif
+
+#include "MFCDialogDoc.h"
+#include "MFCDialogView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCDialogView
+
+IMPLEMENT_DYNCREATE(CMFCDialogView, CView)
+
+BEGIN_MESSAGE_MAP(CMFCDialogView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CMFCDialogView 构造/析构
+
+CMFCDialogView::CMFCDialogView()
+{
+	// TODO:  在此处添加构造代码
+
+}
+
+CMFCDialogView::~CMFCDialogView()
+{
+}
+
+BOOL CMFCDialogView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO:  在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCDialogView 绘制
+
+void CMFCDialogView::OnDraw(CDC* /*pDC*/)
+{
+	CMFCDialogDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO:  在此处为本机数据添加绘制代码
+}
+
+
+// CMFCDialogView 打印
+
+BOOL CMFCDialogView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CMFCDialogView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO:  添加额外的打印前进行的初始化过程
+}
+
+void CMFCDialogView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO:  添加打印后进行的清理过程
+}
+
+
+// CMFCDialogView 诊断
+
+#ifdef _DEBUG
+void CMFCDialogView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCDialogView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCDialogDoc* CMFCDialogView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCDialogDoc)));
+	return (CMFCDialogDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCDialogView 消息处理程序
---
+++ MFCDialog/MFCDialogView.h
@@ -0,0 +1,49 @@
+
+// MFCDialogView.h : CMFCDialogView 类的接口
+//
+
+#pragma once
+
+
+class CMFCDialogView : public CView
+{
+protected: // 仅从序列化创建
+	CMFCDialogView();
+	DECLARE_DYNCREATE(CMFCDialogView)
+
+// 特性
+public:
+	CMFCDialogDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMFCDialogView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MFCDialogView.cpp 中的调试版本
+inline CMFCDialogDoc* CMFCDialogView::GetDocument() const
+   { return reinterpret_cast<CMFCDialogDoc*>(m_pDocument); }
+#endif
+
